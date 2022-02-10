080da2312c314136ee33fd457bfef35ad9baff67
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 19:53:44 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,6 +2,8 @@
 ls
 file="employees.csv"
 pwd
-cat $file
-cd $file
 
+while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
+do
+    echo "INSERT INTO table_name (\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\", \"h\", \"i\", \"j\", \"k\") VALUES ('$col1','$col2','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11');"
+done < employees.csv | sudo psql -U pg_user -d test;
