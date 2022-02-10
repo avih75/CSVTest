fa442a09d836691b7f2d6d24087941baa5cb31dc
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 20:07:59 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -6,4 +6,4 @@ pwd
 while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 do
     echo "INSERT INTO table_name (\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\", \"h\", \"i\", \"j\", \"k\") VALUES ('$col1','$col2','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11');"
-done < $file | sudo psql -U pg_user -d test;
+done < $file | psql -U pg_user -d test;
