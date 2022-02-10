86cc833ab16f99ef8ae68e984df10ef33381c86a
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 18:18:33 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,16 @@
+#!/bin/sh 
+file="filename.php"
+bucket="my-bucket"
+resource="/${bucket}/${file}"
+contentType="text/html"
+dateValue="`date +'%a, %d %b %Y %H:%M:%S %z'`"
+stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key='ABCABCABCABCABCABCAB'
+s3Secret='xyzxyzyxyzxyzxyzxyzxyzxyzxyzxyzxyzxyzxyzx'
+signature=`/bin/echo -en "$stringToSign" | openssl sha1 -hmac ${s3Secret} -      binary | base64`
 ls
+curl -v -H "Host:lssngen-updates-east.s3.amazonaws.com" \
+        -H "Date:${dateValue}" \
+        -H "Content-Type:${contentType}" \
+        -H "Authorization: AWS ${s3Key}:${signature}" \
+        https://${bucket}.s3.amazonaws.com/${file}
