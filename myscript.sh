931224c92a9f238cf84a17e44399bdb0c8a7bc90
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 19:48:41 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,18 +2,6 @@
 ls
 file="employees.csv"
 pwd
-cat $(employees.csv)
-cd employees.csv
-INPUT= employees.csv
-OLDIFS=$IFS
-IFS=,
-[ ! -f $file ] && { echo "$file file not found"; exit 99; }
-while read site post subject usr status
-do
-    echo "Site : $site"
-    echo "Post : $post"
-    echo "Subject : $subject"
-    echo "User : $usr"
-    echo "Status : $status"
-done < $file
-IFS=$OLDIFS
+cat $file
+cd $file
+
