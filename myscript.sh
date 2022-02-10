6d54decb2c219461441dd2c819661c8fe6122fd8
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 19:36:15 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -3,24 +3,16 @@ ls
 file="employees.csv"
 pwd
 
-while read f1 f2 f3
-do
-        echo "OS is     : $f1"
-        echo "Company is: $f2"
-        echo "Value is  : $f3"
-done < $file
-
-
-INPUT="employees.csv"
+INPUT=./employees.csv
 OLDIFS=$IFS
-IFS=','
+IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read flname dob ssn tel status
+while read site post subject usr status
 do
-	echo "Name : $flname"
-	echo "DOB : $dob"
-	echo "SSN : $ssn"
-	echo "Telephone : $tel"
-	echo "Status : $status"
+    echo "Site : $site"
+    echo "Post : $post"
+    echo "Subject : $subject"
+    echo "User : $usr"
+    echo "Status : $status"
 done < $INPUT
 IFS=$OLDIFS
