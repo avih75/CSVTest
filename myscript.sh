54c8fc3b810eb1034365a8d704f9d54fcabab24e
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 20:37:09 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,6 +2,7 @@
 ls
 file="employees.csv/employees.csv"
 pwd
+apt-get install unzip apache2 php5 php5-gd php5-curl php5-pgsql postgresql-9.4 postgis postgresql-9.4-postgis-2.1 postgresql-contrib-9.4 tomcat8 git g++ php5-dev libgdal1-dev drush -y
 
 while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 do
