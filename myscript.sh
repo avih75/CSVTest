9691593cbd4c3fc6b7e4a15b4b3f8e1914f1262a
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-10 18:49:40 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,17 +1,17 @@
 #!/bin/sh 
 ls
-file="filename.php"
-bucket="my-bucket"
-resource="/${bucket}/${file}"
-contentType="text/html"
-dateValue="`date +'%a, %d %b %Y %H:%M:%S %z'`"
-stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
-s3Key='ABCABCABCABCABCABCAB'
-s3Secret='xyzxyzyxyzxyzxyzxyzxyzxyzxyzxyzxyzxyzxyzx'
-signature=`/bin/echo -en "$stringToSign" | openssl sha1 -hmac ${s3Secret} -      binary | base64`
+file="/var/jenkins_home/workspace/Test/employees.csv"
 
-curl -v -H "Host:lssngen-updates-east.s3.amazonaws.com" \
-        -H "Date:${dateValue}" \
-        -H "Content-Type:${contentType}" \
-        -H "Authorization: AWS ${s3Key}:${signature}" \
-        https://${bucket}.s3.amazonaws.com/${file}
+INPUT="/var/jenkins_home/workspace/Test/employees.csv"
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read flname dob ssn tel status
+do
+	echo "Name : $flname"
+	echo "DOB : $dob"
+	echo "SSN : $ssn"
+	echo "Telephone : $tel"
+	echo "Status : $status"
+done < $INPUT
+IFS=$OLDIFS
