27264860f46abc8028f606c4554c9cf4e0c3eee0
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-13 10:24:08 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -7,5 +7,5 @@ echo "CREATE TABLE workers (EMPLOYEE_ID VARCHAR ( 50 ),FIRST_NAME VARCHAR ( 50 )
 title=true;
 while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 do
-   echo "INSERT INTO workers (\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\", \"h\", \"i\", \"j\", \"k\") VALUES ('$col1','$col2','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11');"
+   echo "INSERT INTO workers (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID) VALUES ('$col1','$col2','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11');"
 done < $file | psql $5;
