70474acb1b7a209431ca898a5bdc3751e3dab5e5
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-13 10:26:33 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -9,3 +9,5 @@ while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 do
    echo "INSERT INTO workers (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID) VALUES ('$col1','$col2','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11');"
 done < $file | psql $5;
+
+echo "SELECT * FROM workers" | psql $5;
