1d7c9720815075955f2354f11bc5289eee4059cc
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-13 09:58:24 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,8 +2,8 @@
 ls
 file="employees.csv/employees.csv"
 pwd
-echo "DROP TABLE [IF EXISTS] workers" | psql $5;
-echo "CREATE TABLE [IF NOT EXISTS] workers ('$col1' VARCHAR ( 50 ),'$col2' VARCHAR ( 50 ),'$col3' VARCHAR ( 50 ),'$col4' VARCHAR ( 50 ),'$col5' VARCHAR ( 50 ),'$col6' VARCHAR ( 50 ),'$col7' VARCHAR ( 50 ),'$col8' VARCHAR ( 50 ),'$col9' VARCHAR ( 50 ),'$col10' VARCHAR ( 50 ),'$col11' VARCHAR ( 50 ));" | psql $5;
+echo "DROP TABLE IF EXISTS workers" | psql $5;
+echo "CREATE TABLE workers ('$col1' VARCHAR ( 50 ),'$col2' VARCHAR ( 50 ),'$col3' VARCHAR ( 50 ),'$col4' VARCHAR ( 50 ),'$col5' VARCHAR ( 50 ),'$col6' VARCHAR ( 50 ),'$col7' VARCHAR ( 50 ),'$col8' VARCHAR ( 50 ),'$col9' VARCHAR ( 50 ),'$col10' VARCHAR ( 50 ),'$col11' VARCHAR ( 50 ));" | psql $5;
 title=true;
 while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 do
