3c5ce8a41fcdb755d0f28be422a02cd20e1b3c99
avih75 <35527203+avih75@users.noreply.github.com>
2022-02-13 08:46:37 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -6,4 +6,4 @@ pwd
 while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 do
     echo "INSERT INTO table_name (\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\", \"h\", \"i\", \"j\", \"k\") VALUES ('$col1','$col2','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11');"
-done < $file | psql -h "ec2-15-228-193-93.sa-east-1.compute.amazonaws.com" -p 5432 -U postgres -w;
+done < $file | psql -h $1 -p 5432 -U postgres -w;
